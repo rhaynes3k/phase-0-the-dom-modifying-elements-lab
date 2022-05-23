c266f1ef0efd3aaf98183652c7ac30c76dd286a5
rhaynes3k <rhaynes3k@gmail.com>
2022-05-22 19:45:29 -0500

    commit tree a023abcd475226172bbd1b4116c51bab37c7ea05

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.getElementById('main').remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.textContent = 'Big Bad is the champion'
